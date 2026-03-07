a606f3fb93fe20f3b06cbf379d563e13d81fe7fb
alejozd <alejozd79@gmail.com>
2026-03-06 20:00:31 -0500

    ip de mi pc

--- src/api/axiosClient.js
+++ src/api/axiosClient.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const axiosClient = axios.create({
-  baseURL: "http://localhost:3000/api",
+  // baseURL: "http://localhost:3000/api",
+  baseURL: "http://192.168.1.3:3000/api",
 });
 
 axiosClient.interceptors.request.use((config) => {
